ac5b86b2e608d7baa784353f7f2a1ed510e64332
iamYasmimCarollo <yasmim.cristal13@gmail.com>
2023-09-12 20:32:49 -0300

    feat: adicionando estrutura de pagina de login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,12 +1,66 @@
-import Form from "../../Components/Form";
-function Login() {
+import React, { useState } from 'react';
+import {
+  Button,
+  TextField,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material';
+
+const Login = ({ onLogin }) => {
+  const [cpf, setCpf] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleLogin = () => {
+    onLogin(cpf, senha);
+  };
+
   return (
-    <div>
-      <h1>
-        <Form/>
-      </h1>
-    </div>
-  )
-}
+    <Container maxWidth="xs">
+      <Grid
+        container
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: '100vh' }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h4" align="center">
+            Login
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="CPF"
+            variant="outlined"
+            value={cpf}
+            onChange={(e) => setCpf(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Senha"
+            variant="outlined"
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={handleLogin}
+          >
+            Entrar
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default Login;
