ed73a33a666965e3980d0ee739a4ed4ca2ab14ee
iamYasmimCarollo <yasmim.cristal13@gmail.com>
2023-09-12 22:15:07 -0300

    adicionando a pagina inicial de cadastro de usuario

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,66 +1,104 @@
-import React, { useState } from 'react';
-import {
-  Button,
-  TextField,
-  Container,
-  Grid,
-  Typography,
-} from '@mui/material';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ImageFinanca from "../../img/financas.jpg";
+import Logo from "../../img/logo.png";
 
-const Login = ({ onLogin }) => {
-  const [cpf, setCpf] = useState('');
-  const [senha, setSenha] = useState('');
+const defaultTheme = createTheme();
 
-  const handleLogin = () => {
-    onLogin(cpf, senha);
+export default function LoginPage() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      cpf: data.get('cpf'),
+      senha: data.get('senha'),
+    });
   };
 
   return (
-    <Container maxWidth="xs">
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        alignItems="center"
-        style={{ height: '100vh' }}
-      >
-        <Grid item xs={12}>
-          <Typography variant="h4" align="center">
-            Login
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="CPF"
-            variant="outlined"
-            value={cpf}
-            onChange={(e) => setCpf(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Senha"
-            variant="outlined"
-            type="password"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={handleLogin}
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+          backgroundImage: ` url(${ImageFinanca}) `,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
           >
-            Entrar
-          </Button>
+           <img
+        src={`${Logo}`}height={'250'} width={'250'}/> 
+            <Typography component="h1" variant="h5">
+            Login
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="cpf"
+                label="CPF"
+                name="cpf"
+                autoComplete="cpf"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="password"
+                type="password"
+                id="password"
+                autoComplete="password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
         </Grid>
       </Grid>
-    </Container>
+    </ThemeProvider>
   );
-};
-
-export default Login;
+}
