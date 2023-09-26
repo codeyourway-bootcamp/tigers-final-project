70a0caa3cda29ab045c4d8da9cfc72cd2a99cc3b
jca_amaral <jca_amaral@hotmail.com>
2023-09-26 06:32:07 -0300

    Renderizando o Footer

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,13 +1,14 @@
 import Navbar from "../../Components/Navbar";
 import Form from "../../Components/Form";
 import PageContainer from "../../Components/PageContainer";
-
+import Footer from "../../Components/Footer";
 function Login() {
   return (
     <div>
       <Navbar isTransparent={false} isLandingPage={false} />
       <PageContainer>
         <Form />
+        <Footer simple={true} />
       </PageContainer>
     </div>
   );
