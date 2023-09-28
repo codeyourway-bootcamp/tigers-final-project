5e68e73d32649019c2957b67128dde8c61b77c37
diegogmedeiros <diego.mck@hotmail.com>
2023-09-28 20:12:21 -0300

    att footer erro

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -2,10 +2,7 @@ import BottomNavigate from "../../Components/BottomNavigate";
 import CardProfile from "../../Components/CardProfile";
 import Navbar from "../../Components/Navbar";
 import PageContainer from "../../Components/PageContainer";
-<<<<<<< HEAD
 import Footer from "../../Components/Footer";
-=======
->>>>>>> 6327b8fb0b7ffb5b35ca35fd3407449bf6404bd0
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
 import Footer from "../../Components/Footer";
 
