b64db4e1c5d5b87b530c1ffb431cab3420044d3c
jaklim <jaklimcaleia@icloud.com>
2023-09-28 21:17:13 +0100

    Adicionando o componente na pagina Chart

--- src/Pages/Charts/index.jsx
+++ src/Pages/Charts/index.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../../Components/Navbar";
 import BottonNavigate from "../../Components/BottomNavigate";
 import PageContainer from "../../Components/PageContainer";
+import ExpenseBarChart from "../../Components/Charts/ExpensesBarChart";
 
 function Charts() {
   return (
     <div>
       <Navbar isTransparent={false} isLandingPage={false} />
       <PageContainer>
+      <ExpenseBarChart />
         <h1>Charts</h1>
         <BottonNavigate />
       </PageContainer>
