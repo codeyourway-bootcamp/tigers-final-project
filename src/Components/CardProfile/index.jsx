388a8292c6494aa81ec87c6e399a45c8657e201e
diegogmedeiros <diego.mck@hotmail.com>
2023-09-28 19:29:17 -0300

    capturando dados da api

--- src/Components/CardProfile/index.jsx
+++ src/Components/CardProfile/index.jsx
@@ -1,40 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+
 //import './style.css';
-import { Card, CardHeader, CardBody, CardFooter, Divider, flexbox, Heading, Stack, StackDivider, Box, Text } from '@chakra-ui/react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  flexbox,
+  Heading,
+  Stack,
+  StackDivider,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import { user_registration } from "../../FakeDatabase";
 
 // import { Container } from './styles';
 
- function CardProfile() {
+function CardProfile() {
+  const [userData, setUserData] = useState({
+    user_first_name: "",
+    user_email: "",
+  });
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const loggedUser = localStorage.getItem("app@user");
+    if (loggedUser) {
+      console.log(loggedUser, JSON.parse(loggedUser));
+      setUser(JSON.parse(loggedUser));
+    }
+  },[]);
+  useEffect(() => {
+    user_registration.then((data) => {
+      const filteredData = data.data.filter((item) => {
+        return item.user_email === user;
+      });
+      console.log(filteredData);
+      setUserData(filteredData[0]);
+    });
+  },[user]);
   return (
     <Card>
-  <CardHeader>
-    <Heading size='md'>Account</Heading>
-  </CardHeader>
+      <CardHeader>
+        <Heading size="md">Account</Heading>
+      </CardHeader>
 
-  <CardBody>
-    <Stack divider={<StackDivider />} spacing='4'>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'>
-          Nome
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          Diego Gonçalves de Medeiros
-        </Text>
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'>
-          Email
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          email.teste@gmail.com
-        </Text>
-      </Box>
-      
-    </Stack>
-  </CardBody>
-</Card>
-    
-    
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Nome
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {userData.user_first_name}
+            </Text>
+          </Box>
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Email
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {userData.user_email}
+            </Text>
+          </Box>
+        </Stack>
+      </CardBody>
+    </Card>
   );
 }
 
