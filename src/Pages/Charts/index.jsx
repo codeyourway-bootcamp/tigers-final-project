a99128e8f59cdffc4948b56b4e7da9ff7555427d
Ivonel <ivonel.am94@gmail.com>
2023-10-12 20:20:55 +0100

    Criando gráfico pizza

--- src/Pages/Charts/index.jsx
+++ src/Pages/Charts/index.jsx
@@ -5,6 +5,7 @@ import Footer from "../../Components/Footer";
 import BalanceTracker from "../../Components/BottonExpenses";
 import { Box } from "@chakra-ui/react";
 import ExpenseBarChart from "../../Components/Charts/ExpensesBarChart";
+import { ChartPie } from "../../Components/ChartPie";
 
 function Charts() {
   const pageStyles = {
@@ -18,6 +19,7 @@ function Charts() {
     fontSize: "24px",
     paddingLeft: "20px",
     marginTop: "20px",
+    marginLeft: "20px",
   };
 
   return (
@@ -27,8 +29,13 @@ function Charts() {
         <BalanceTracker />
         <Box className="charts-content">
           <h2 style={styleH2}>Gráficos</h2>
-          <Box className="Box-charts">
-            <ExpenseBarChart />
+          <Box className="box-charts">
+            <Box className="box-expenser">
+              <ExpenseBarChart />
+            </Box>
+            <Box className="box-chartpie">
+              <ChartPie />
+            </Box>
           </Box>
           <BottomNavigate />
         </Box>
